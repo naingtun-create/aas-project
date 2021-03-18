681ac2adf1c52f9b6bf1687af7435c822ed7b932
aliciateo <e0420622@u.nus.edu>
2021-03-18 15:52:43 +0800

    profile

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HomePage msg="Testing"></HomePage>
+    <ProfilePage msg="Testing Alicia"></ProfilePage>
   </div>
 </template>
 
 <script>
 import HomePage from './components/HomePage.vue'
+import ProfilePage from './components/ProfilePage.vue'
 
 export default {
   name: 'App',
   components: {
     HomePage:HomePage,
+    ProfilePage:ProfilePage,
   }
 }
 </script>
