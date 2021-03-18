c500199a3869f8a215883507feaadb0c6e9dacd6
brandaphoon <brandaphoon05@gmail.com>
2021-03-18 17:41:19 +0800

    Updated LogIn

--- src/components/SignUpPage.vue
+++ src/components/SignUpPage.vue
@@ -1,14 +1,14 @@
 <template>
     <div id ="SignUpPage">
         <p>Hello </p>
-        <p>My name is Branda </p>
+        <p>My name is Branda</p>
     </div>
 </template>
 
 <script>
 export default {
   name: 'SignUp Page',
-
+  
 }
 </script>
 
