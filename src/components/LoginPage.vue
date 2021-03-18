4405f5e682d9445dc93e7e465fef832d256641f1
brandaphoon <brandaphoon05@gmail.com>
2021-03-18 18:10:59 +0800

    Added Vuetify

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,5 +1,8 @@
 <template>
     <div id="LogInPage">
+        <h3>Welcome back!</h3>
+        <h1>Log in Page</h1> 
+
     </div> 
 </template>
 
--- src/components/SignUpPage.vue
+++ src/components/SignUpPage.vue
@@ -1,14 +1,16 @@
 <template>
     <div id ="SignUpPage">
-        <p>Hello </p>
+        <p>Hello</p>
         <p>My name is Branda</p>
+        <v-btn
+          elevation="2"
+        >Click me</v-btn>
     </div>
 </template>
 
 <script>
 export default {
   name: 'SignUp Page',
-  
 }
 </script>
 
