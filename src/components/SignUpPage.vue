7bdfc0438f15f187dcb1e42939eda451320c50a2
Shudan <shudan@7.8.126.129.static.m1net.com.sg>
2021-03-18 19:10:26 +0800

    update signup page

--- src/components/SignUpPage.vue
+++ src/components/SignUpPage.vue
@@ -1,18 +1,32 @@
 <template>
     <div id = "SignUpPage">
         <h2> Get started with All About Sustainability </h2>
-        <label for = "email"> Email Address </label><br>
-        <input type = "email" id = "email" name = "email"><br>
-        <label for = "password"> Password </label><br>
-        <input type = "password" id = "pwd" name = "pwd" minlength = "8"><br>
-        <button> Sign Up </button>
+        <label for = "email"> Email Address </label><br><br>
+        <input type = "email" id = "email" name = "email" v-model="email"><br><br>
+        <label for = "password"> Password </label><br><br>
+        <v-text-field type="password" rounded = "true" color = "purple" v-model="email"></v-text-field><br><br>
+        <v-btn> Sign Up </v-btn><br><br>
         <p>Already have account?</p>
+        <v-btn v-on:click = "$router.push('login')"> Login </v-btn>
     </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'SignUpPage',
+  data(){
+    return{
+      email:"",
+      password:"",
+    }
+  },
+  methods:{
+    sendAccount:function(){
+
+    }
+  },
   components: {
 
   }
