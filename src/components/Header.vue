3825eec14cd1a5a2453e6167abd7d4624af0f245
aliciateo <e0420622@u.nus.edu>
2021-03-19 10:27:11 +0800

    header alignment

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
     <div id="navbar">
       <img src="https://pics.freeicons.io/uploads/icons/png/18453753581581665319-512.png" class="logo">
-      <ul> All About Sustainability</ul>
+      <ul class="logo"> All About Sustainability</ul>
+      <div class="links">
       <ul><router-link to="/" exact>Home</router-link></ul>
       <ul><router-link to="/physicalstore" exact>Physical Store</router-link></ul>
       <ul><router-link to="/onlinemarketplace" exact>Online Marketplace</router-link></ul>   
       <ul><router-link to="/incubator" exact>Incubator</router-link></ul>
       <ul><router-link to="/login" exact>Login</router-link></ul>
       <ul><router-link to="/signup" exact>Sign Up</router-link></ul>
+      </div>
     </div>
 </template>
 
@@ -22,15 +24,22 @@ export default {
 <style>
 #navbar {
   display: flex;
-  text-align:center;
+  padding-top: 0px;
   color:white;
   background-color:#c9AA88;
   height:100px;
-  font-size: 35px;
+  font-size:35px;
+  padding-top: 0px;
   font-family: "Sanchez";
 }
 .logo {
   height:80px;
   padding-left: 50px;
+  
+}
+.links{
+  padding-left: 40%;
+  text-align: center;
+  display:inline-flex;
 }
 </style>
