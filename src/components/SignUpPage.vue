b5c1f352cc5cb8b65c7ff62999acf167344274ac
Shudan <shudan@Admins-MacBook-Pro.local>
2021-03-20 13:36:49 +0800

    add identity property of users in signUp

--- src/components/SignUpPage.vue
+++ src/components/SignUpPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div id = "SignUpPage">
-        <h2> Get started with All About Sustainability </h2>
+        <h1> Get started with All About Sustainability </h1>
         <v-card-text>
             <v-form>
                 <v-text-field label="Email Address" type="email" filled v-model = "email"></v-text-field>
@@ -8,9 +8,25 @@
             </v-form>
         </v-card-text>
 
-        <v-btn color="blue" v-on:click = "sendAccount"> Sign Up </v-btn><br><br>
+        <v-chip large class="ma-2" color="#FFECB3" label v-on:click="typeSelected('Shopper')">
+            <v-icon left> mdi-account-circle-outline </v-icon>
+            Shopper
+        </v-chip>
+
+        <v-chip large class="ma-2" color="#FFECB3" label v-on:click="typeSelected('Company')">
+            <v-icon left> mdi-domain </v-icon>
+            Company
+        </v-chip>
+
+        <v-chip large class="ma-2" color="#FFECB3" label v-on:click="typeSelected('Admin')">
+            <v-icon left> mdi-wrench </v-icon>
+            Admin
+        </v-chip>
+        <p id = "identity" v-show = "selected"> Identity Selected: <b>{{type}}</b> </p><br><br>
+
+        <v-btn x-large color="blue" v-on:click = "sendAccount"> Sign Up </v-btn><br><br><br><br>
         <p>Already have account?</p>
-        <v-btn v-on:click = "$router.push('login')" color="blue"> Login </v-btn>
+        <v-btn x-large v-on:click = "$router.push('login')" color="blue"> Login </v-btn>
     </div>
 </template>
 
@@ -23,6 +39,8 @@ export default {
     return{
       email:"",
       password:"",
+      type:"",
+      selected:false,
     }
   },
   methods:{
@@ -30,15 +48,28 @@ export default {
       var user = {
         "email":this.email,
         "password": this.password,
+        "type": this.type,
       }
       db.collection('users').add(user).then(() => location.reload())
+    },
+    typeSelected:function(identity){
+      this.selected = true;
+      this.type = identity;
     }
   },
   components: {
-
+    
   }
 }
 </script>
 
 <style scoped>
+h1 {
+  font-family: "monospace", Times, serif;
+  font-size: 60px;
+}
+
+#identity{
+ font-size: 30px; 
+}
 </style>
---
+++ src/components/SignUpSuccessful.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id = "SignUpPage">
+        <h2> Get started with All About Sustainability </h2>
+        <v-card-text>
+            <v-form>
+                <v-text-field label="Email Address" type="email" filled v-model = "email"></v-text-field>
+                 <v-text-field label="Password" type="password" filled v-model = "password"></v-text-field>
+            </v-form>
+        </v-card-text>
+
+        <v-chip class="ma-2" color="primary" label>
+            <v-icon left> mdi-account-circle-outline </v-icon>
+            Shopper
+         </v-chip>
+
+        <v-btn color="blue" v-on:click = "sendAccount"> Sign Up </v-btn><br><br>
+        <p>Already have account?</p>
+        <v-btn v-on:click = "$router.push('login')" color="blue"> Login </v-btn>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'SignUpSuccessfulPage',
+  data(){
+    return{
+    }
+  },
+  methods:{
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
