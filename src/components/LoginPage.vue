1c9f8578065c13422d09aaeb49c5839ebf2ac3f3
naingtun-create <60058845+naingtun-create@users.noreply.github.com>
2021-03-23 12:15:24 +0800

    changed signuppage to register

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,21 +4,23 @@
             <v-card-title>Login to your Account</v-card-title>
             <v-card-text>
                 <v-form>
-                    <v-text-field label="Email"></v-text-field>
-                    <v-text-field label="Password"></v-text-field>
+                    <v-text-field label="Email" v-model="email"></v-text-field>
+                    <v-text-field label="Password" v-model="password"></v-text-field>
                 </v-form>
             </v-card-text>
             <v-card-actions>
+                <v-form @submit.prevent="login">
                 <v-btn
                 color="#B3E5FC"
                 class="mr-4"
-                @click="reset"><router-link to="/profile" exact>Login</router-link></v-btn>
+                @click="login">Login</v-btn>
+                </v-form>
                 <v-btn
                 color="#EF9A9A"
                 @click="resetValidation"><router-link to="/resetpassword" exact>Reset Password</router-link></v-btn>
                 <v-btn
                 color="#B2DFDB"
-                @click="resetValidation"><router-link to="/signup" exact>Sign Up</router-link></v-btn>
+                @click="resetValidation"><router-link to="/register" exact>Sign Up</router-link></v-btn>
 
             </v-card-actions>
         </v-card>
@@ -26,15 +28,29 @@
 </template>
 
 <script>
-
+import firebase from 'firebase';
 export default {
-    data: function() {
+    data() {
         return {
-          
-        }
+            email: '',
+            password: '',
+        };
     },
-
-}
+    methods: {
+        login() {
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.email, this.password)
+                .then(() => {
+                    alert('Successfully logged in');
+                    this.$router.push('/profile');
+                })
+                .catch(error => {
+                    alert(error.message);
+                });
+        },
+    },
+};
 </script>
 
 <style scoped>
