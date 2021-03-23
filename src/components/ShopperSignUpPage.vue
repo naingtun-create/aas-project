80ee5b8aa1546a0b67382bea1e01288d10585ead
brandaphoon <brandaphoon05@gmail.com>
2021-03-23 19:03:07 +0800

    Added dedicated pages for each user type

---
+++ src/components/ShopperSignUpPage.vue
@@ -0,0 +1,82 @@
+<template>
+   <div id="ShopperSignUpPage">
+       <v-card-title>Get started with All About Sustainability!</v-card-title>
+       <v-card-text>
+           <v-form @submit.prevent="register">
+               <v-text-field
+               type="email"
+               label="Email address..."
+               v-model="email"></v-text-field>
+               <v-text-field
+                label="Full Name"
+                v-model="fullName"
+               > 
+               </v-text-field>
+               <v-text-field
+               type="password"
+               label="Password..."
+               v-model="password"></v-text-field>
+               <v-btn
+                color="#B3E5FC"
+                class="mr-4"
+                type="submit">Register</v-btn>
+               </v-form>
+               </v-card-text>
+
+               <p>Already have account?</p>
+        <v-btn x-large v-on:click = "$router.push('shopperlogin')" color="blue"> Login </v-btn>
+</div> 
+</template>
+
+<script>
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import db from '../firebase.js';
+
+
+export default {
+    name: 'Register',
+    data() {
+        return {
+            fullName: '',
+            email: '',
+            password: '',
+        };
+    },
+    methods: {
+        register() {
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.email, this.password)
+                .then(() => {
+
+                    const user = firebase.auth().currentUser;
+                    
+                    user.updateProfile({
+                        displayName: this.fullName
+                    }).then(() => {
+                        db.collection('users').doc(user.uid).set({
+                            fullname: this.fullName,
+                            email: this.email,
+                            type: "Shopper"
+                        })
+                    }).then(()=> {
+                        this.$router.push('home');
+                    }).catch(error => {
+                        alert(error.message)
+                    })
+                    
+                    //const actionCodeSettings = {
+                      //  url: `${process.env.VUE_APP_HOST_NAME}/sign-in/?email=${user.email}`,
+                        //};
+                    user.sendEmailVerification();
+                    alert('Successfully registered! Please login.');
+                    this.$router.push('/');
+                })
+                .catch(error => {
+                    alert(error.message);
+                });
+        },
+    },
+};
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import LoginPage from './components/LoginPage.vue'
-import HomePage from './components/HomePage.vue'
+import CompanyLoginPage from './components/CompanyLoginPage.vue'
+import ShopperLoginPage from './components/ShopperLoginPage.vue'
+import CompanySignUpPage from './components/CompanySignUpPage.vue'
+import ShopperSignUpPage from './components/ShopperSignUpPage.vue'
+//import HomePage from './components/HomePage.vue'
 import SignUpPage from './components/SignUpPage.vue'
 import ProfilePage from './components/ProfilePage.vue'
 import PhysicalStore from './components/PhysicalStore.vue'
@@ -10,12 +14,18 @@ import ResetPassword from './components/ResetPassword.vue'
 import ResetSuccessful from './components/ResetSuccessful.vue'
 import CompanyPage from './components/CompanyPage.vue'
 import Register from './components/Register.vue'
+import LoginDir from './components/LoginDirectory.vue'
 
 
 export default [
-    { path: '/', component: HomePage},
+    //{ path: '/', component: HomePage},
+    { path: '/', component: LoginDir},
     { path: '/login', component: LoginPage},
+    { path: '/companyLogin', component: CompanyLoginPage},
+    { path: '/shopperlogin', component: ShopperLoginPage},
     { path: '/signup', component: SignUpPage},
+    { path: '/companysignup', component: CompanySignUpPage},
+    { path: '/shoppersignup', component: ShopperSignUpPage},
     { path: '/profile', component: ProfilePage, meta: { requiresAuth: true}},
     { path: '/physicalstore', component: PhysicalStore},
     { path: '/onlinemarketplace', component: OnlineMarketplace},
