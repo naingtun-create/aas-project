a638c71838451bb4e66b104609033eb5e5738951
naingtun-create <60058845+naingtun-create@users.noreply.github.com>
2021-03-31 12:03:52 +0800

    Modified

--- src/components/shopper/ProfilePage.vue
+++ src/components/shopper/ProfilePage.vue
@@ -1,3 +1,71 @@
 <template>
-    <p> this is profile </p>
-</template>>
+    <div id="profilepage">
+        <h1>Header</h1>
+        <h2>Testing</h2>
+        <h3>Hello World</h3>
+        <div style="margin-left: 0px; margin-top:100px; position: aboslute;right:150px;">
+            <p>Testing</p>
+        </div>
+    </div>
+</template>
+
+
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;800;900&display=swap');
+#profilepage {
+  position: relative;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100%;
+}
+h1 {
+  text-align: left;
+  padding-left: 20px;
+  color: rgb(8, 8, 8);
+  font-family: Nunito;
+  font-weight: bolder;
+  font-size: 40px;
+}
+h2 {
+  margin: -20px;
+  font-size: 34px;
+  font-family: Nunito;
+  font-weight: bolder;
+  margin-top:75px;
+  color: rgb(8, 31, 37);
+}
+h3 {
+  margin: -20px;
+  font-size: 34px;
+  font-family: Nunito;
+  font-weight: bolder;
+  margin-top:175px;
+  color: rgb(16, 240, 210);
+}
+h4 {
+  font-size: 20px;
+  font-family: Nunito;
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  margin-top: -20px;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  font-family: Nunito;
+  font-weight: bold;
+  color: rgb(0, 114, 180);
+  text-decoration: none;
+}
+.router-link-active {
+  color:white
+}
+</style>
