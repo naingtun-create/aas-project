e813169a345f266d84dc498573cd036ed0d1db5b
Shudan <shudan@r-74-104-25-172.comp.nus.edu.sg>
2021-04-13 13:53:15 +0800

    Complete order function in company order page

--- src/components/shopper/Payment.vue
+++ src/components/shopper/Payment.vue
@@ -106,7 +106,7 @@ export default {
               totalPrice: 0,
               cartID:[],
               companyID:'',
-              completed:false,
+              //completed:false,
             }
           }
 
