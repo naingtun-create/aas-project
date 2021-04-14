44cde7fae1049be833452b0cb411cb2bdcf15d3b
naingtun-create <60058845+naingtun-create@users.noreply.github.com>
2021-04-14 19:40:43 +0800

    changed product page

--- src/components/company/companyProductPage.vue
+++ src/components/company/companyProductPage.vue
@@ -3,7 +3,7 @@
     <company-header></company-header>
     <div id="intro">
         <img id="arrow" src='../../assets/arrow.png'>
-        <router-link id="arrow" to="/company/onlinemarketplace" exact>Back to Online MarketPlace</router-link>
+        <router-link id="back" to="/company/companyproducts" exact>Back to My Products</router-link>
         <br><br>
         <p> {{this.datapacket[0].category}} <p>
         <p id="title"> {{this.datapacket[0].title}}</p>
@@ -12,18 +12,18 @@
         <v-img v-bind:src="this.datapacket[0].image"></v-img>
     </div>
     <div id="contents">
-        <p id="bold"> View Seller: {{this.datapacket[0].companyName}}</p>
-        <p id="bold"> Price:</p> SGD {{this.datapacket[0].price}} <br>
-        <p id="bold">Additional Information: </p> {{this.datapacket[0].sizeguide}}
+        <p id="bold"> Price:</p> 
+        <p id="details">SGD {{this.datapacket[0].price}} </p>
+        <p id="bold">Additional Information: </p>
+        <p id="details">{{this.datapacket[0].sizeguide}}</p>
         <p v-if="this.datapacket[0].size" id="bold"> Select Size: 
             <v-select v-model="selectedSize" :items="this.datapacket[0].size" filled label="Size" dense ></v-select>
         </p>
-        <br>
         <p id="bold"> Colours Available:</p>
-        <p v-for="color in this.datapacket[0].colors" :key="color.id">{{color}}</p>
+        <p id="details" v-for="color in this.datapacket[0].colors" :key="color.id">{{color}}</p>
 
         <p id="bold"> Sizes Available:</p>
-        <p v-for="size in this.datapacket[0].sizings" :key="size.id">{{size}}</p>
+        <p id="details" v-for="size in this.datapacket[0].sizings" :key="size.id">{{size}}</p>
     </div> 
   </div>
 </template>
@@ -93,18 +93,18 @@ export default {
 
 <style scoped>
 #intro {
-    width:50%;
+    width:40%;
     float:left;
     padding-top:50px;
-    padding-left:200px;
-    font-size:30px;
+    padding-left:100px;
+    font-size:20px;
     text-align:left;
 }
 #contents {
     width:40%;
-    padding-top:130px;
+    padding-top:50px;
     float:right;
-    font-size:40px;
+    font-size:20px;
     margin: 100px;  
 }
 #bold{
@@ -113,29 +113,39 @@ export default {
 }
 #title{
     font-weight: bold;
-    font-size:80px;
+    font-size:30px;
     font-family: 'Anton', sans-serif;
     color:#c9AA88;
 }
 #select{
     color:#c9AA88;
-    font-size:40px;
+    font-size:15px;
 }
 button {
-  width: 400px;
-  height: 120px;
+  width: 200px;
+  height: 80px;
   background-color: #c9AA88;
   border-radius: 10px;
   border-width: 1px;
 }
 input[type=number] {
     background-color: #ececec;
-    font-size:40px;
+    font-size:20px;
 }
 #arrow {
-    height:60px;
-    font-size:40px;
+    height:30px;
+    font-size:20px;
     color:black;
+    margin-top:-10px;
+}
+#back {
+    height:30px;
+    font-size:20px;
+    color:black;
+    margin-top:-15px;
+}
+#details {
+    margin-top: -15px;
 }
 </style>
 
